b53ecd444fa42f79f73c332bf161a83a310e8e56
leekyoungminimi <leekyoungminimi@gmail.com>
2019-03-14 14:57:32 +0900

    test입니다

---
+++ app/src/main/java/com/example/myapplication/LLLLLLLLLLLL.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class LLLLLLLLLLLL {
+
+    private String a;
+    private String b;
+
+    public LLLLLLLLLLLL(String a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+}
